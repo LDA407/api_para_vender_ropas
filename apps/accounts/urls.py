62a5e588ecdec11af15cc68615340b8692296f57
LDA407 <88114886+LDA407@users.noreply.github.com>
2023-05-25 01:37:44 -0300

    eliminando apps innecesarias

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,4 +3,13 @@ from django.urls import path
 app_name='accounts'
 
 urlpatterns = [
+	path('get_items', GetItemsView.as_view()),
+	path('add_item', AddItemView.as_view()),
+	path('get_items_total', GetItemTotalView.as_view()),
+	path('remove_item', RemoveItemView.as_view()),
+	path('<int:productId>', views.GetReview.as_view()),
+    path('new/<int:productId>', views.CreateReview.as_view()),
+    path('review', views.UpdateReview.as_view()),
+    path('del/<int:productId>', views.DeleteReview.as_view()),
+    path('filter/<int:productId>', views.FilterReview.as_view()),
 ]
--- apps/coupons/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CheckCouponView
-
-
-app_name='coupons'
-urlpatterns = [
-	path('coupon', CheckCouponView.as_view()),
-]
