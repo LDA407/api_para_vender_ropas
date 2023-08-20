e0683824c5d5364e3240093991db12cd3c6a1ed7
LDA407 <88114886+LDA407@users.noreply.github.com>
2023-08-20 18:38:17 -0300

    Update urls.py

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 app_name='product'
 
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+router.register('taxes', views.TaxViewSet)
+
 urlpatterns = [
     path('categories', views.ListCategoriesView.as_view()),
-    path('list', views.ProductListView.as_view()),
-    path('search', views.SearchProductView.as_view()),
-    path('search/filtered', views.FilterBySearchView.as_view()),
-    path('detail/<int:id>', views.DetailProductView.as_view()),
+    path('', views.ProductListView.as_view()),
+    path('discounts', views.ListDiscountView.as_view()),
+    path('detail/<int:id>', views.DetailProductView.as_view(), name="detail"),
     path('related/<int:productId>', views.ListRelatedView.as_view()),
+    path('', include(router.urls))
 ]
