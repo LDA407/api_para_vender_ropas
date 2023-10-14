e70cb692ee0c98d7b9d209ed76ab661fdceebfae
lda407 <davidlencinaok@gmail.com>
2023-10-13 23:14:35 -0300

    Update scripts.py

--- utils/scripts.py
+++ utils/scripts.py
@@ -1,17 +1,29 @@
 import json, random
 from apps.product.models import Product, Category
 from django.shortcuts import get_object_or_404
-filename ="C:\\Users\\LDA407\\PythonProjects\\ecommers_react\\backend\\utils\\product.json"
+filename ="/home/lda_pc/PythonProyects/api_para_vender_ropas/utils/products.json"
 # C:\Users\LDA407\PythonProjects\ecommers_react\backend\utils\product.json
+
+categories = '/home/lda_pc/PythonProyects/api_para_vender_ropas/utils/categories.json'
+with open(categories, 'r', encoding="utf8") as f: 
+    data = json.load(f)
+    for item in data:
+        c = Category.objects.create(
+            name = item["name"]
+        )
+        c.save()
+
 
 with open(filename, 'r', encoding="utf8") as f: 
     data = json.load(f)
     for item in data:
         p = Product.objects.create(
-            name = item["title"],
+            name = item["name"],
             description = item["description"],
             price = item["price"],
+            category = get_object_or_404(Category ,id=item["category"]),
             quantity=random.randint(50, 500),
-            category = get_object_or_404(Category ,name=item["category"])
+            sold = item["sold"],
+            date_created = item["date_created"]
         )
         p.save()
